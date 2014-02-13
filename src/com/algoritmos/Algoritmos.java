68008af668d067aac7fc1c1087a7b3b931ff4d17
gutoomota <h2mario@gmail.com>
2014-02-13 14:53:33 -0200

    Correção Fecho Transitivo

--- src/com/algoritmos/Algoritmos.java
+++ src/com/algoritmos/Algoritmos.java
@@ -25,7 +25,7 @@ public class Algoritmos {
 			System.out.println("3 - Obter arvore de Kruskal");
 			System.out.println("4 - Obter caminho de Dijkstra");
 			System.out.println("5 - Obter arvore de Busca em Profundidade");
-			System.out.println("6 - Obter Fecho Transitivo de um vertice em tal Grafo");
+			System.out.println("6 - Obter Fecho Transitivo do Grafo");
 			System.out.println("7 - Obter arvore de Busca em Largura");
 			System.out.println("8 - Obter Ordenacao Topologica");
 			System.out.println("9 - Obter Matriz do Algoritmo de Warshall");
@@ -90,11 +90,23 @@ public class Algoritmos {
 			case 6:
 				//Algoritmo de Fecho Transitivo
 				
-				origem = Keyboard.readString();
-				resultado.setVertices(inicial.fechoTransitivo(origem));
+				//para cada vertice do Grafo
+				for(int i=0;i<inicial.getVertices().size();i++){
+					//para cada vertice do seu fecho transitivo
+					for(int j=0;j<inicial.fechoTransitivo(inicial.getVertices().get(i).getNome()).size();j++){
+						arestaAux = resultado.acharAresta(inicial.getVertices().get(i), inicial.fechoTransitivo(inicial.getVertices().get(i).getNome()).get(j)); 
+						//verifica se uma aresta com esse vertice e o vertice inicial já existe
+						//se não existe
+						if(arestaAux==null){
+							//adiciona aresta
+							resultado.addAresta(0,
+									inicial.getVertices().get(i).getNome(),
+									inicial.fechoTransitivo(inicial.getVertices().get(i).getNome()).get(j).getNome() );
+						}
+					}
+				}
 				
-				System.out.println(resultado.getVertices());
-				resultado.imprimeArvore();
+				System.out.println(resultado.getArestas());
 				break;
 			case 7:
 				//Algoritmo de Busca em Largura
